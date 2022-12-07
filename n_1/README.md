9303fb9fecb8a0c487a5dceb7020a3d02a5494dc
Estefanie Coleone <84871976+estefaniecoleone@users.noreply.github.com>
2022-12-07 02:55:39 -0300

    subindo primeira versao

---
+++ n_1/README.md
@@ -0,0 +1,4 @@
+# LP2atividade
+Problema n+1
+
+- Link direto: https://github.com/Laridesa/LP2atividade/tree/main/src/main
---
+++ n_1/src/main/sql/dadossql.sql
@@ -0,0 +1,9 @@
+INSERT INTO Proprietario(id, nome) VALUES (1, 'Rafael');
+INSERT INTO Proprietario(id, nome) VALUES (2, 'Cristina');
+INSERT INTO Proprietario(id, nome) VALUES (3, 'Helena');
+
+INSERT INTO Veiculo(id, modelo, codigo_proprietario) VALUES (1, 'Uno',1);
+INSERT INTO Veiculo(id, modelo, codigo_proprietario) VALUES (2, 'Strada',1);
+INSERT INTO Veiculo(id, modelo, codigo_proprietario) VALUES (3, 'Celta',2);
+INSERT INTO Veiculo(id, modelo, codigo_proprietario) VALUES (4, 'Hilux',3);
+INSERT INTO Veiculo(id, modelo, codigo_proprietario) VALUES (5, 'Gol',3);
